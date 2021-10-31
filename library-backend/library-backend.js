cb420d688ec02ddc5de3c57742f969f8743c5ab5
Wilson Diaz <wdfiguereo@gmail.com>
2021-10-31 17:29:58 -0400

    8.4: all books with author param

--- library-backend/library-backend.js
+++ library-backend/library-backend.js
@@ -107,7 +107,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -116,7 +116,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      return !args.author 
+        ? books
+        : books.filter(b => b.author === args.author)
+    },
     allAuthors: () => authors
   },
   Author: {
